c21383a1efa66f9d1ceab42d79aa0bf08cda6fed
g4ze <bhaiyajionline17@gmail.com>
2023-08-09 11:30:54 +0530

    Create q1.py

---
+++ probability lab/lab2/q1.py
@@ -0,0 +1,11 @@
+import numpy as num
+import collections as colections
+import random
+print("start")
+arr=[]
+for i in range(1500):
+    arr.append((random.randint(90,100)))
+elements_count=colections.Counter(arr)
+for key, value in elements_count.items():
+    print(f"{key} : {value}")
+
