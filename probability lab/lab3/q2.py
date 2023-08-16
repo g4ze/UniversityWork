da3a2298d6734d3bd9816db0f1413d474c55e6e8
g4ze <bhaiyajionline17@gmail.com>
2023-08-16 11:43:25 +0530

    done

--- probability lab/lab3/q2.py
+++ probability lab/lab3/q2.py
@@ -1,7 +1,18 @@
 import numpy as num
-import collections as colections
 # now we would colaculate the probability of atleast one heads in 4 coin tosses
 samplespace=2**4
-pnohead=1/samplespace
-ponehead=1-pnohead
-print(ponehead)
+outcomes = [
+    "1111", "1110", "1101", "1100",
+    "1011", "1010", "1001", "1000",
+    "0111", "0110", "0101", "0100",
+    "0011", "0010", "0001", "0000"
+]
+
+head_counts = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0}
+
+for outcome in outcomes:
+    head_count = outcome.count('1')
+    head_counts[head_count] += 1
+
+for n in range(5):
+    print(f"X with {n} heads: {head_counts[n]/16}")
