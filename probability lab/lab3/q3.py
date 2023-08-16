b42d86eb27b84e2f8b16796bdc42eb3f4a5a708b
g4ze <bhaiyajionline17@gmail.com>
2023-08-16 11:27:09 +0530

    q2 done

--- probability lab/lab3/q3.py
+++ probability lab/lab3/q3.py
@@ -1,3 +1,7 @@
 import numpy as num
 import collections as colections
 # now we would colaculate the probability of atleast one heads in 4 coin tosses
+samplespace=2**4
+pohead=1/samplespace
+ponehead=1-pohead
+print(ponehead)
